f5e892c5911c71880888a268b2e85f6f5f09e520
Mises Dev <misesdev@onionmail.org>
2024-10-02 17:48:41 -0300

    find users and friends

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use illuminate\http\request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     function index(string $pubkey)
     {
+        $validate = Validator::make([ 'pubkey' => $pubkey ], [
+            'pubkey' => ['required', 'max:64', 'min:64']
+        ]);
 
+        if($validate->fails())
+        {
+            return response()->json($validate->errors(), 403);
+        }
+
+        $response = Http::post("http://192.168.0.11:8080/get_user", $validate->valid());
+
+        return $response->json();
     }
 
     function friends(string $pubkey)
     {
+        $validate = Validator::make([ 'pubkey' => $pubkey ], [
+            'pubkey' => ['required', 'max:64', 'min:64']
+        ]);
 
+        if($validate->fails())
+        {
+            return response()->json($validate->errors(), 403);
+        }
+
+        $response = Http::post("http://192.168.0.11:8080/get_friends", $validate->valid());
+
+        return $response->json();
     }
 }
+
+
+
